203584d43d865c7c3ca44b685a65b65ddf09f1ed
tokaji <fpzk5656@gmail.com>
2021-12-07 17:19:26 +0900

    입력한 맵 내용을 저장하고, 가로, 세로, 공, 구멍, 플레이어 위치 등을 2차원 배열에 저장하는 것은 잘 된듯.
    todo 루프문 탈출만

--- sokobanGame/src/fpzk5656/sokobanGame/Stage1.java
+++ sokobanGame/src/fpzk5656/sokobanGame/Stage1.java
@@ -4,69 +4,20 @@ import java.util.Scanner;
 
 public class Stage1 {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-		// 문자열 받아온 것을 2차원 배열에 저장하거나 출력한다.
-		// 맵의 한정크기를 처음에 입력 받지 않으므로 배열 크기는 임시로 정한다.
-		int[][] tale = new int[9999][9999];
-		int countY = 0;
-		int countX = 0;
-		int horizontal = 0;
-		int[] horizontalGroup = new int[999];
-		int vertical = 0;
+	public static void print(int[][] tale, int[] horizontalGroup, int horizontal, int vertical) {
 		int hole = 0;
 		int ball = 0;
 		int playerX = 0;
 		int playerY = 0;
-
-		while (vertical < 4) {
-			String input = scanner.nextLine();
-			System.out.println(input);
-
-			if (horizontal < input.length())
-				horizontal = input.length();
-			horizontalGroup[vertical] = input.length();
-
-			// 문자열의 문자를 하나하 쪼개서 분석하기
-			for (int i = 0; i < input.length(); i++) {
-				char n = input.charAt(i);
-				switch (n) {
-				case '#':
-					tale[i][vertical] = 0;
-					break;
-				case 'O':
-					tale[i][vertical] = 1;
-					break;
-				case 'o':
-					tale[i][vertical] = 2;
-					break;
-				case 'P':
-					tale[i][vertical] = 3;
-					break;
-				case '=':
-					tale[i][vertical] = 4;
-					break;
-				case ' ':
-					tale[i][vertical] = 7; // 잠시만 7로 해놓는거
-					break;
-				}
-			}
-
-			vertical++;
-		}
 		int count = 0;
 		while (true) {
-			if(horizontalGroup[count] == 0)break;
-			
-			//for (int i = 0; i < horizontal; i++) {
-			for(int i = 0; i < vertical; i++) {
+			if (horizontalGroup[count] == 0)
+				break;
+
+			for (int i = 0; i < vertical; i++) {
 				for (int j = 0; j < horizontalGroup[count]; j++) {
-					int k = horizontalGroup[count];
-					System.out.println(tale[j][i]);
-					
-					switch(tale[j][i])
-					{
+
+					switch (tale[j][i]) {
 					case 1:
 						hole++;
 						break;
@@ -86,7 +37,106 @@ public class Stage1 {
 		System.out.println("세로크기: " + vertical);
 		System.out.println("구멍의 수: " + hole);
 		System.out.println("공의 수: " + ball);
-		System.out.printf("플레이어 위치 (%d, %d)",playerX,playerY);
-		
+		System.out.printf("플레이어 위치 (%d, %d)\n", playerY, playerX);
 	}
+
+	public static void calculate(Scanner scanner) {
+		int horizontal = 0;
+		int vertical = 0;
+		int count = 0;
+		int[][] tale = new int[999][999];
+		int[] horizontalGroup = new int[999];
+		boolean nextStage = false;
+		while (true) {
+			
+			String input = "";
+			
+			if(scanner.findInLine("(?=\\S)") != null)
+			{
+				
+				if(scanner.hasNextLine())
+				{
+					input = scanner.nextLine();
+				}
+				else
+				{
+					break;
+				}
+			}
+			else
+			{
+				break;
+			}
+			
+			
+			
+			
+			
+			
+			
+			if (!input.contains("=")) {
+				System.out.println(input);
+				if(input.contains("Stage"))
+				{
+					System.out.printf("\n");
+				}
+			}
+			else
+			{
+				// 구분선이 있다면 다음 스테이지가 있다는 얘기니까 활성화 됨
+				nextStage = true;
+			}
+
+			// 요녀석을 boolean값으로 만들어서 함수에 넣는 식으로 하는 거지
+			if (!input.contains("#") && !input.contains("Stage")) {
+				break;
+			}
+			if (input.indexOf("#") > -1) {
+
+				if (horizontal < input.length())
+					horizontal = input.length();
+				horizontalGroup[vertical] = input.length();
+
+				// 문자열의 문자를 하나하 쪼개서 분석하기
+				for (int i = 0; i < input.length(); i++) {
+					char n = input.charAt(i);
+					switch (n) {
+					case '#':
+						tale[i][vertical] = 0;
+						break;
+					case 'O':
+						tale[i][vertical] = 1;
+						break;
+					case 'o':
+						tale[i][vertical] = 2;
+						break;
+					case 'P':
+						tale[i][vertical] = 3;
+						break;
+					case '=':
+						tale[i][vertical] = 4;
+						break;
+					case ' ':
+						tale[i][vertical] = 7; // 예외처리용
+						break;
+					}
+				}
+				vertical++;
+			}
+		}
+
+		print(tale, horizontalGroup, horizontal, vertical);
+		if(nextStage)
+		{
+			nextStage = false;
+			calculate(scanner);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		calculate(scanner);
+
+	}
+
 }
